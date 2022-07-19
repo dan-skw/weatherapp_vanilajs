a120aae960ca31933f0d4813ae598ba45150bbad
dan-skw <s223628@student.uek.krakow.pl>
2022-07-19 17:44:00 +0200

    a

--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -1,21 +1,23 @@
 const fetch = require("node-fetch");
-const { json } = require("stream/consumers");
 
-const { WEATHER_API_KEY } = process.env;
+const { WEATHER_API_KEY } = "9167868d2b570ca315b9dd2a7da25be5";
 
 exports.handler = async (event, context) => {
   const params = JSON.parse(event.body);
-  const { text, units } = paramas;
+  const { text, units } = params;
   const regex = /^\d+$/g;
   const flag = regex.test(text) ? "zip" : "q";
   const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${text}&units=${units}&appid=${WEATHER_API_KEY}`;
   const encodedUrl = encodeURI(url);
 
   try {
-    dataStream = await fetch(encodedUrl);
-    jsonData = await dataStream.json();
+    const dataStream = await fetch(encodedUrl);
+    const jsonData = await dataStream.json();
 
-    return { statusCode: 200, body: JSON.stringify(jsonData) };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(jsonData),
+    };
   } catch (err) {
     return { statusCode: 422, body: err.stack };
   }
